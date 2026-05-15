bfc37b645219499acb9607c5abd5523270e7dd12
Jango <psyea6@nottingham.ac.uk>
2026-05-15 23:43:15 +0100

    created inits

---
+++ src/beamng_rl/track/__init__.py
@@ -0,0 +1,6 @@
+"""Track loading and query helpers for BeamNG RL environments."""
+
+from .query_utils import TrackCentreline, TrackQueryResult, heading_error
+
+__all__ = ["TrackCentreline", "TrackQueryResult", "heading_error"]
+
