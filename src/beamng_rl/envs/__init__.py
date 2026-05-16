0af125dc86e7fd86831a457998e36f02ee4718a0
Emman <psyea6@nottingham.ac.uk>
2026-05-16 09:13:07 +0100

    created fully commented observation_builder.py

---
+++ src/beamng_rl/envs/__init__.py
@@ -0,0 +1,5 @@
+"""Environment-facing integration helpers for BeamNG RL."""
+
+from .observation_builder import ObservationBuilder, ObservationConfig
+
+__all__ = ["ObservationBuilder", "ObservationConfig"]
